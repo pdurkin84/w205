7762be632e64ef26b2f5b8f98776075c2416bead
pdurkin84 <durkin_paul@yahoo.co.uk>
2017-10-29 13:51:13 +0000

    Updated the best states query

--- exercise_1/investigations/best_states/best_states.sql
+++ exercise_1/investigations/best_states/best_states.sql
@@ -1,4 +1,7 @@
-!echo "A biAS result AS any hospitals with only 1 procedure can have 100% if that one procedure is good"
-SELECT AVG(hospitalScores.mean) AS mean,hospitals.state AS state FROM hospitals JOIN hospitalScores ON hospitals.providerid = hospitalScores.providerid GROUP BY hospitals.state ORDER BY mean DESC LIMIT 10;
-!echo "A less biAS result choosing hospitals that have at least 10 procedures to choose from"
-SELECT AVG(HospitalScores.mean) AS mean,hospitals.state AS state FROM hospitals JOIN hospitalScores ON hospitals.providerid = hospitalScores.providerid WHERe hospitalscores.numprocedures>10 GROUP BY hospitals.state ORDER BY mean DESC LIMIT 10;
+SELECT 
+	hospitals.stateName,
+	sum(HospitalScores.totalscore)/sum(HospitalScores.numProcedures) as stateMean,
+	sum(HospitalScores.totalscore) AS stateTotal,
+	sum(HospitalScores.numProcedures) as stateProcTotal
+	FROM hospitals JOIN hospitalScores ON hospitals.providerid = hospitalScores.providerid 
+GROUP BY hospitals.stateName ORDER BY stateMean DESC LIMIT 10;

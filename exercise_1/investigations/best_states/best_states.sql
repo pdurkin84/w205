8e0cd97d7d0181a4e00d8051ef702cdae59bc9c0
pdurkin84 <durkin_paul@yahoo.co.uk>
2017-10-29 10:19:21 +0000

    Updates

--- exercise_1/investigations/best_states/best_states.sql
+++ exercise_1/investigations/best_states/best_states.sql
@@ -1,4 +1,4 @@
-!echo "A bias result as any hospitals with only 1 procedure can have 100% if that one procedure is good"
-select avg(hospitalScores.mean) as mean,hospitals.state as state from hospitals join hospitalScores on hospitals.providerid = hospitalScores.providerid group by hospitals.state order by mean;
-!echo "A less bias result choosing hospitals that have at least 10 procedures to choose from"
-select avg(hospitalScores.mean) as mean,hospitals.state as state from hospitals join hospitalScores on hospitals.providerid = hospitalScores.providerid where hospitalscores.numprocedures>10 group by hospitals.state order by mean desc limit 10;
+!echo "A biAS result AS any hospitals with only 1 procedure can have 100% if that one procedure is good"
+SELECT AVG(hospitalScores.mean) AS mean,hospitals.state AS state FROM hospitals JOIN hospitalScores ON hospitals.providerid = hospitalScores.providerid GROUP BY hospitals.state ORDER BY mean DESC LIMIT 10;
+!echo "A less biAS result choosing hospitals that have at least 10 procedures to choose from"
+SELECT AVG(HospitalScores.mean) AS mean,hospitals.state AS state FROM hospitals JOIN hospitalScores ON hospitals.providerid = hospitalScores.providerid WHERe hospitalscores.numprocedures>10 GROUP BY hospitals.state ORDER BY mean DESC LIMIT 10;
